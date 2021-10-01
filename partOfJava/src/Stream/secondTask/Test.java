7863324f623d1421d8f23290d10d2ce5a1e79d38
Kiryl.Arlou <kirillorlov1205@gmail.com>
2021-10-01 12:10:01 +0300

    Streams added

--- partOfJava/src/Stream/secondTask/Test.java
+++ partOfJava/src/Stream/secondTask/Test.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 public class Test {
 	public static void main(String[] args) {
 		ArrayList<String> strings = new ArrayList<>();
-		Collections.addAll(strings, "kirierie","dfsfd","sfdsdf", "var");
+		Collections.addAll(strings, "kirierie", "dfsfd", "sfdsdf");
 
 		System.out.println(SecondTask.secondTask(strings));
 	}
