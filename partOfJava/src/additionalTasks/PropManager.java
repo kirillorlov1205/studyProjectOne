022f566c874cdde146c3a15b9264c7a8faa17967
kirillorlov1205 <kirillorlov1205@gmail.com>
2022-01-27 23:40:03 +0300

    add PropManager

---
+++ partOfJava/src/additionalTasks/PropManager.java
@@ -0,0 +1,37 @@
+package additionalTasks;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropManager {
+//    private static final String PATH_TO_PROPERTIES_FILE = "src/main/resources/UserData.property.properties";
+//    private static final Properties props;
+//
+//    static {
+//        props = new Properties();
+//        LoadProperties();
+//    }
+//
+//    private static void LoadProperties() {
+//        try (InputStream in = new FileInputStream(PATH_TO_PROPERTIES_FILE)) {
+//            Logger.getLogger().debug("Loading property file {}", PATH_TO_PROPERTIES_FILE);
+//            props.load(in);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public static String getProperty(String key) {
+//        String property = System.getenv(key);
+//        if (property == null) {
+//            property = props.getProperty(key);
+//        }
+//        Logger.getLogger().debug("Return value {} for property key {}", property, key);
+//        return property;
+//    }
+//
+
+}
+
